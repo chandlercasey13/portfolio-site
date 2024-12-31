3f5a1dc904aaf1e84e04ce869848fea73121dd3d
Chandler Casey <chandlercasey13@gmail.com>
2024-12-31 07:05:07 -0600

    Add work experience and about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 
 import BlurFade from "@/components/ui/blur-fade";
 import { FadeText } from "@/components/ui/fade-text";
+
+import { ResumeCard } from "@/components/resume-card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { data } from "@/data/resume";
-
+import Markdown from "react-markdown";
 
 
 
@@ -36,6 +38,54 @@ export default function Home() {
               </Avatar>
             </BlurFade>
           </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className=" max-w-full text-pretty  text-sm ">
+            {data.summary}
+          </Markdown>
+          <div className="flex flex-wrap gap-4 mt-4 ">
+            {data.contact.social.map((social) => (
+              <div
+                key={social.url}
+                className="flex gap-1 justify-center items-center"
+              >
+                <social.icon className="size-4" />
+                <a href={social.url} target="_blank" rel="noreferrer">
+                  {social.name}
+                </a>
+              </div>
+            ))}
+          </div>
+        </BlurFade>
+      </section>
+      <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {data.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+              />
+            </BlurFade>
+          ))}
         </div>
       </section>
       </main>
